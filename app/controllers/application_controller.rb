6ae3f0022a0edfe020b62402617a001486a3862d
smamadgi6 <swathi.mamadgi@gmail.com>
2020-08-12 13:14:08 -0400

    Added extended forecast such as main weather and weather description

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,12 +15,10 @@ class ApplicationController < ActionController::Base
         
         parsed_address = address.address_parse(query_string)
          if parsed_address[:city].nil? or parsed_address[:city].empty?
-
           puts "City not found"
-
           @error = "City not found"
           return
-        end  
+         end  
         city = parsed_address[:city]
         zip_code = parsed_address[:zipcode]
         @city = city
@@ -41,7 +39,14 @@ class ApplicationController < ActionController::Base
         @current_temperature = weather["main"]["temp"]
         @min_temperature = weather["main"]["temp_min"]
         @max_temperature = weather["main"]["temp_max"]
-
+        weather_first = weather["weather"].first
+        if weather_first.nil?
+          @weather_main = ''
+          @weather_desc = ''
+        else
+        @weather_main =  weather_first["main"]
+        @weather_desc =  weather_first["description"]
+        end
 
       rescue StandardError => e
         puts e.message
